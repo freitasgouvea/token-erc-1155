5ca7cf57c9bee3c5b9ae654dfb4347408f6981d8
Flavio Gouvea <flaviogouveaneto@gmail.com>
2024-07-08 21:56:37 -0300

    forge init

---
+++ src/TokenErc1155.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import { ERC1155 } from "../lib/solmate/src/tokens/ERC1155.sol";
+import { Owned } from "../lib/solmate/src/auth/Owned.sol";
+
+contract TokenERC1155 is ERC1155, Owned {
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol
+    ) ERC1155(tokenName, tokenSymbol) Owned(msg.sender) {}
+
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public virtual {
+        _mint(to, id, amount, data);
+    }
+
+    function batchMint(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual {
+        _batchMint(to, ids, amounts, data);
+    }
+
+    function burn(
+        address from,
+        uint256 id,
+        uint256 amount
+    ) public virtual {
+        _burn(from, id, amount);
+    }
+
+    function batchBurn(
+        address from,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public virtual {
+        _batchBurn(from, ids, amounts);
+    }
+}
